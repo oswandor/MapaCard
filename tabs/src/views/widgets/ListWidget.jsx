e80c0bb8e97e81dec5613cd27bcd18ce6ab04c9f
oswandor <oswandor26@gmail.com>
2023-03-09 17:13:14 -0600

    first commit

---
+++ tabs/src/views/widgets/ListWidget.jsx
@@ -0,0 +1,85 @@
+import { Button } from "@fluentui/react-components";
+import { getListData } from "../../services/listService";
+import { Widget } from "../lib/Widget";
+import MapView from "../MapBox";
+import {bodyContentStyle} from "../styles/ListWidget.styles";
+import * as microsoftTeams from "@microsoft/teams-js";
+
+
+/**
+ * Extends the Widget class to implement a list widget.
+ */
+export class ListWidget extends Widget {
+  /**
+   * Get data required by the widget, you can get data from a api call or static data stored in a file.
+   * @returns The data required by the widget to render.
+   */
+   constructor(){
+
+      
+   }
+
+
+  async getData() {
+    return getListData();
+  }
+
+  /**
+   * Define the widget body.
+   * @returns The body content, all JSX.Element types are supported.
+   */
+  bodyContent() {
+
+    return (
+      <div style={bodyContentStyle()}>
+        <MapView></MapView>
+      </div>
+    );
+  }
+
+ 
+  /**
+   * Define the widget footer.
+   * @returns The footer content, all ReactNode types are supported.
+   */
+  footerContent() {
+    
+    if (!microsoftTeams?.initialize) {
+      console.error("Teams no está disponible en este entorno");
+      return null;
+    }
+  
+    microsoftTeams.initialize();
+  
+
+    const handleButtonClick = () => {
+
+     
+    let locationProps = {"allowChooseLocation":true,"showMap":true};
+
+if(microsoftTeams.geolocation.isSupported()) {
+    const locationPromise = microsoftTeams.geolocation.getLocation(locationProps);
+    locationPromise
+        .then((result) => {console.log(JSON.stringify(result));})
+        .catch((error) => {console.log(error);});
+}
+else {
+    /* Manejar caso en el que la capacidad no está soportada */
+}
+
+     
+    };
+
+   //const handleButtonClick = ()=> { console.log("Hellos")}
+
+    return (
+      <Button
+        appearance="primary"
+        size="medium"
+        onClick={handleButtonClick} // add the event handler to the button
+      >
+        Obtener Ubicacion
+      </Button>
+    );
+  }
+}
